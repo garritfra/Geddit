8e5a5c1c8feb1416afe8abb59015d3de2b5e11d9
garritfra <garritfranke@gmail.com>
2018-04-28 18:08:54 +0200

    add initial boilerplate

---
+++ src/Screens/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Button, Text } from 'native-base';
+export default class Main extends Component {
+    render() {
+        return (
+            <Container>
+                <Button>
+                    <Text>
+                        Button
+                    </Text>
+                </Button>
+            </Container>
+        );
+    }
+}
